1ab57bc306f8edc784c328f409438228a7553072
Mario Blanco Dominguez <56127361+running004@users.noreply.github.com>
2021-03-27 17:17:13 +0100

    first commit

---
+++ src/main/java/grapp/grapp/GrappApplication.java
@@ -0,0 +1,13 @@
+package grapp.grapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grapp/grapp/GrappApplicationTests.java
@@ -0,0 +1,13 @@
+package grapp.grapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
