6608a4ac27f393c6810105a87eb5cd7c18969815
Mario Blanco Dominguez <56127361+running004@users.noreply.github.com>
2021-03-29 17:04:08 +0200

    Formularios ++

--- src/main/java/grapp/grapp/appController.java
+++ src/main/java/grapp/grapp/appController.java
@@ -1,22 +1,27 @@
 package grapp.grapp;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Controller
-public class appController {
+public class appController implements ErrorController{
 
     @GetMapping(value= "/")
     String index(Model model){
@@ -30,7 +35,8 @@ public class appController {
     }
 
     @GetMapping(value="/form")
-    String form(Model model){
+    String form(Model model,@Valid Formulario formulario){
+        //model.addAttribute("formulario", new Formulario());
         return "form.html";
     }
 
@@ -39,8 +45,20 @@ public class appController {
         if(bindingResult.hasErrors()){
             return "form.html";
         }
-        model.addAttribute("confirmation", "He recibido el POST y los campos estan bien");
+        //model.addAttribute("img", formulario.getImg().toURI());
+        model.addAttribute("confirmation", "He recibido el POST y los campos estan bien. Pusiste:" +formulario.getText());
         return "form.html";
+    }
+
+    @RequestMapping(value = "/error")
+    public String error() {
+        return "Error handling";
+    }
+
+    @Override
+    public String getErrorPath() {
+        // TODO Auto-generated method stub
+        return "/error";
     }
 
 }
